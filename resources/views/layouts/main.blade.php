f93d669b7fb2d5ba2ad401546dc0b29c86144ad2
robialakbar <robialakbar@gmail.com>
2024-07-30 15:55:55 +0700

    udate

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta charset="utf-8" />
-	<title>Primaya Hospital Bhakti Wara | {{ config('app.name', 'Demo') }} - @yield('title')</title>
+	<title>Masaksih | {{ config('app.name', 'Demo') }} - @yield('title')</title>
 	<meta name="description" content="overview &amp; stats" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
