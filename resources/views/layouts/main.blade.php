3ea1942423871f479d0b295ddc25c6d0a1147b53
Desman Harianto Pardosi <desman@pardosi.net>
2024-08-04 14:13:27 +0700

    Terbaru

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,7 @@
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta charset="utf-8" />
-<<<<<<< HEAD
-	<title>Masaksih | {{ config('app.name', 'Demo') }} - @yield('title')</title>
-=======
 	<title>{{ config('app.name', 'Demo') }} - @yield('title')</title>
->>>>>>> 8e8aea5 (Terbaru)
 	<meta name="description" content="overview &amp; stats" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
